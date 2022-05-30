12a695ef88ec526412b7ee544b9a35e63eac66df
Rafal Fuz <rafalfuz@gmail.com>
2022-05-30 15:29:18 +0200

    kkk

---
+++ tests/ad.test.ts
@@ -0,0 +1,9 @@
+import { AdRecord } from "../records/ad.record";
+
+test('AdRecord returns data from database for one entry', async ()=>{
+    const ad = await AdRecord.getOne('abc')
+    
+    expect(ad).toBeDefined();
+    expect(ad.id).toBe('abc');
+    expect(ad.title).toBe('test')
+})
